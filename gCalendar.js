e02d08b2e55bc819b48411c2294156596cff5687
ayortanli <ahmetyortanli@gmail.com>
2021-02-18 15:28:09 +0300

    Calendar content section was determined in both popup and fullscreen view

--- gCalendar.js
+++ gCalendar.js
@@ -1,1 +1,14 @@
-document.body.style.border = "5px solid red";
+const body = document.querySelector('body');
+
+const config = {childList: true, subtree: true };
+
+const callback = function(mutationsList, observer) {
+    console.log("Dom tree changed");
+    var tabEvent = document.querySelector('#tabEvent') || document.querySelector('#tabEventDetails');
+    if(tabEvent)
+        tabEvent.style.border = "5px solid red";        
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(body, config);
+
