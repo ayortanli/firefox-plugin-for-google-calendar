75d34582bd87dffa4c6dbf39c45d4bbe57fa9317
ayortanli <ahmetyortanli@gmail.com>
2021-02-21 00:40:21 +0300

    For each view, video conference button holder node was found and cloned for used as our new button holder

--- src/CalendarViewModifier.js
+++ src/CalendarViewModifier.js
@@ -1,19 +1,53 @@
 
 export default class CalendarViewModifier {   
-    editPopupView(containerNode) {
-        containerNode.style.border = "5px solid red"; 
+
+    constructor(){
+        this.newComponentId = "my-new-button";
     }
 
+    /**
+     * --ContainerNode
+     *      --FirstChild
+     *          --SchedularNode
+     *          --TimeNode
+     *          --AttendeeNode
+     *          --MeetingNode (with attribute 'data-is-meet-selected')
+     *          --LocationNode
+     */
+    editPopupView(containerNode) {
+        containerNode = containerNode.firstChild;//unnecessary
+        var meetingNode = containerNode.querySelector("[data-is-meet-selected]");
+        var newNode = meetingNode.cloneNode(true);
+        newNode.setAttribute("id", this.newComponentId);
+        meetingNode.after(newNode);
+        //bunun icini temizle ve bir id ver.// Bu id'li eleman varsa hiç girme çizim işine
+        //sonra bunu toplantıdan sonraya merge et.
+        newNode.style.border = "5px solid red"; 
+    }
+
+    /**
+     * --ContainerNode
+     *      --MeetingNode (doesn't have an indikator attribure :( )
+     *      --LocationNode
+     *      --NotificationNode
+     */
     editFullScreenView(containerNode) {
-        containerNode.style.border = "5px solid red"; 
+        var meetingNode = containerNode.firstChild;
+        var newNode = meetingNode.cloneNode(true);
+        newNode.setAttribute("id", this.newComponentId);
+        meetingNode.after(newNode);
+                
+        newNode.style.border = "5px solid red"; 
     }
 
     editView(node) {
+        if(node.querySelector(`#${this.newComponentId}`)) //if inserted already
+            return
         var container = node.querySelector("#tabEvent") || node.querySelector("#tabEventDetails");
-            if(container)
-                if(container.getAttribute("id")==="tabEvent")
-                    this.editPopupView(container);
-                else
-                    this.editFullScreenView(container);
+        if(container)
+            if(container.getAttribute("id")==="tabEvent")
+                this.editPopupView(container);
+            else
+                this.editFullScreenView(container);
     }
 }
--- src/CalendarViewModifier.test.js
+++ src/CalendarViewModifier.test.js
@@ -8,27 +8,35 @@ describe("CalendarViewModifier viewModify method", () => {
     popUpView.setAttribute("id", "tabEvent");
 
     const fullScreenView = document.createElement("div");
-    fullScreenView.setAttribute("id", "tabEventDetails");
+    fullScreenView.setAttribute("id", "tabEventDetails"); 
 
-    it("calls related modify method when popupView/fullscreenView is active in calendar", done => {
-        let modifier = new CalendarViewModifier();
+    const modifiedView = document.createElement("div");
+    modifiedView.setAttribute("id", "my-new-button"); 
+
+    it("calls related modify method when popupView/fullscreenView is active in calendar and not already modified", () => {
+        let modifier = new CalendarViewModifier();        
         modifier.editFullScreenView = jest.fn();
         modifier.editPopupView = jest.fn();
 
         modifier.editView(container);  //nothing to edit
         expect(modifier.editFullScreenView.mock.calls.length).toBe(0);
-        expect(modifier.editPopupView.mock.calls.length).toBe(0);
+        expect(modifier.editPopupView.mock.calls.length).toBe(0);        
 
+        container.appendChild(modifiedView);
+        modifier.editView(container);  //already modified so nothing to edit
+        expect(modifier.editFullScreenView.mock.calls.length).toBe(0);
+        expect(modifier.editPopupView.mock.calls.length).toBe(0);
+        
+        container.removeChild(modifiedView);
         container.appendChild(popUpView);
-        modifier.editView(container);  //nothing to edit
+        modifier.editView(container);  //call popupView Node Finder
         expect(modifier.editFullScreenView.mock.calls.length).toBe(0);
         expect(modifier.editPopupView.mock.calls.length).toBe(1);
 
         container.removeChild(popUpView);
         container.appendChild(fullScreenView);
-        modifier.editView(container);  //nothing to edit
+        modifier.editView(container);  //call fullscreenView Node Finder
         expect(modifier.editFullScreenView.mock.calls.length).toBe(1);
         expect(modifier.editPopupView.mock.calls.length).toBe(1);
-        done(); //why?
     });
 });
